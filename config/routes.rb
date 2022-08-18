f2253ca655272ae85b236db9d4ce4d26b65cf048
777v56 <107239556+777v56@users.noreply.github.com>
2022-08-18 15:29:22 +0000

    routes2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :customers, skip: [:passwords], controllers:{
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -8,34 +9,28 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
 
-
  scope module: :public do
    root to: "homes#top"
-   get 'home/about' => 'homes#about' 
+   get 'home/about' => 'homes#about'
+
    resources :items, only: [:index,:show]
-   resource :customers do
-     collection do
-       get :unsubscribe 
-       patch :withdraw
-     end
-   end
-   resources :customers, only: [:show,:edit,:update]
-   resources :cart_items do
-     collection do
-       delete :destroy_all
-     end
-   end
+
+   get 'customers/unsubscribe' => 'customers#unsubscribe'
+   patch 'customers:withdraw' => 'customers#withdraw'
+   get 'customers/my_pege' => 'customers#show'
+   get 'customers/information/edit' => 'customers#edit'
+   patch 'customers/information' => 'customers#update'
+
    resources :cart_items, only: [:index,:update,:destroy,:create]
-   resources :orders do
-     collection do
-       post :confirm
-       get :done
-     end
-   end
+   delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+
    resources :orders, only: [:new,:create,:index,:show]
+   post 'orders/confirm' => 'orders#confirm'
+   get 'orders/done' => 'orders#done'
+
    resources :addresses, only: [:index,:edit,:create,:update,:destroy]
  end
- 
+
  namespace :admin do
    root to: "homes#top"
    resources :items, only: [:index,:new,:create,:show,:edit,:update]
