92e8579da0ceb1247a1139b07bfce0d089089461
777v56 <107239556+777v56@users.noreply.github.com>
2022-08-19 11:34:16 +0000

    add admin login

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  devise_for :admin, skip: [:registrations, :passwords], controllers:{
+  devise_for :admin, skip: [:registrations,:passwords], controllers:{
     sessions: "admin/sessions"
   }
  scope module: :public do
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
    resources :items, only: [:index,:new,:create,:show,:edit,:update]
    resources :genres, only: [:index,:create,:edit,:update]
    resources :customers, only: [:index,:show,:edit,:update]
-   resources :orders, only: [:show,:update]
+   resources :orders, only: [:index,:show,:update]
    resources :order_details, only: [:update]
  end
 end
