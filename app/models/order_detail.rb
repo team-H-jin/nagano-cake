ca39737bf4e91c314db5d730141de2c9bf21bca4
777v56 <107239556+777v56@users.noreply.github.com>
2022-08-21 09:28:00 +0000

    tax

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,11 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
-  
+
   enum status: { impossible_manufacture: 0, waiting_manufacture: 1, manufacturing: 2, finish: 3 }
+
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
 end
