a4f796a348506bea6fa85048f108219205941426
EC2 Default User <ec2-user@ip-172-31-1-148.ap-northeast-1.compute.internal>
2022-08-21 04:56:03 +0000

    あ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,8 +4,12 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
   
-  
   def unsubscribe
     @customer = current_customer
+  end
+  
+  
+  def customer
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:address,:postcode,:phone_number)
   end
 end
