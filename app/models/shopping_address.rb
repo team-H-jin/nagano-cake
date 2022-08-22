c4dcda434e1e033838e0031db2929bc2dcc29778
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-08-22 08:55:58 +0000

    8/22

--- app/models/shopping_address.rb
+++ app/models/shopping_address.rb
@@ -1,3 +1,11 @@
 class ShoppingAddress < ApplicationRecord
   belongs_to :customer
+  
+  def new
+  @address = ShoppingAddres
+  end 
+  
+end
+def address_display
+  '〒' + postal_code + ' ' + address + ' ' + name
 end
