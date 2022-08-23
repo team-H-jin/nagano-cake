f32705142c4a5c24ba8683a9b602204c495346c9
minakatatakeru <ec2-user@ip-172-31-43-59.ec2.internal>
2022-08-23 06:32:46 +0000

    途中(takeru)

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,12 @@ class Admin::OrderDetailsController < ApplicationController
     
  def show
     @customer = Customer.find(params[:id])
+    @order_details = Order_details.find(params[:id])
  end
     
+ private  
+ 
+ def order_detail_params
+    params.require(:order_detail).permit(:last_name,:first_name,)
+ end   
 end
