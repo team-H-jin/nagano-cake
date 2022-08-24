afc99384261d4a7cbb90cb8068ecf79d7abec614
777v56 <107239556+777v56@users.noreply.github.com>
2022-08-24 11:32:45 +0000

    注文情報入力＆確認ページの訂正とコントローラーの記述(全編)

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,118 +1,55 @@
 class Public::OrdersController < ApplicationController
 
- def new
-   @order = Order.new
-
-
-
-
-
-
-
-
-
- end
-
-  def done
-
-
-
-
-
-
-
-
-
-
+  def new
+    @order = Order.new
   end
 
-  def comfirm
+  def confirm
     @cart_items = current_customer.cart_items.all
-    @total = @cart_items.inject(0) { |sum, item| sum + item.gross }
+    @sum = 0
+    @cart_items.each do |cart_item|
+      @sum += cart_item.sum_price
+    end
+    @total = @sum + postage
 
     @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
 
+    if params[:order][:selected_address] == "customer_address"
+      @order.postcode = current_customer.postcode
+      @order.name = current_customer.full_name
+      @order.address = current_customer.address
 
+    elsif params[:order][:selected_address] == "shopping_address"
+      @address = ShoppingAddress.find(params[:order][:address_id])
+      @order.postcode = @address.postcode
+      @order.address = @address.address
+      @order.name = @address.name
 
+    else params[:order][:selected_address] == "new_address"
 
-
-
-
-
-
+    end
   end
 
   def create
+    redirect_to orders_done_path
+  end
 
-
-
-
-
-
-
-
-
-
+  def postage
+    postage = 800
   end
 
   def index
-
-
-
-
-
-
-
-
-
-
-
-  end
-
-  def show
-
-
-
-
-
-
-
-
-
-
-
-    @order = Order.find(params[:id])
-    @order.order_details
-
-    @subtotal=
-      @sum = 0
-      @order.order_details.each do |order_detail|
-      @sum += detail.quantity * order_detail.price
-      end
-
-    @total = @subtotal += 800
   end
 
   def show
     @order = Order.find(params[:id])
-    @order.order_details
-
-    @subtotal=
-      @sum = 0
-      @order.order_details.each do |order_detail|
-      @sum += detail.quantity * order_detail.price
-      end
-
-    @total = @subtotal += 800
   end
 
   private
 
   def order_params
-  	params.require(:order).permit(:name, :address, :postcode, :postage, :price, :payment_method, :status, :created_at, :customer_id)
+  	params.require(:order).permit(:name, :address, :postcode, :postage,
+  	                              :price, :payment_method, :status,
+  	                              :created_at, :customer_id, :item_id, :quantity)
   end
 end
