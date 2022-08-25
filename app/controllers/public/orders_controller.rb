cd6dbfb2e2cf704d11b275a8e2638d361f67f2b1
minakatatakeru <ec2-user@ip-172-31-43-59.ec2.internal>
2022-08-25 04:38:29 +0000

    orders_controller修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,29 +2,7 @@ class Public::OrdersController < ApplicationController
 
  def new
    @order = Order.new
-
-
-
-
-
-
-
-
-
  end
-
-  def done
-
-
-
-
-
-
-
-
-
-
-  end
 
   def confirm
     @customer = current_customer
@@ -33,7 +11,6 @@ class Public::OrdersController < ApplicationController
     @cart_items.each do |cart_item|
     @sum += cart_item.sum_price
     end
-
     @order = Order.new(order_params)
     @address = Address.find(params[:order][:address_id])
     @order.postal_code = @address.postal_code
@@ -41,26 +18,27 @@ class Public::OrdersController < ApplicationController
     @order.name = @address.name
 
 
-
-
-
-
-
-
-
   end
 
   def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    if @order.save
+       cart_items.each do |cart|
+      order_detail = OrderDetail.new
+      order_detail.item_id = cart.item_id
+      order_detail.order_id = @order.id
+      order_detail.quantity = cart.quantity
+      order_detail.price = cart.item.price
+      order_detail.save
+    end
+    cart_items.destroy_all
+    redirect_to　orders_done_path
+    else
+    @order = Order.new(order_params)
+    render :new
 
-
-
-
-
-
-
-
-
-
+    end
   end
 
   def index
